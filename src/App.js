ca52afde073e0b4f86d2157e92c56827d409d591
sangeetha-ram <sangiammu1020@gmail.com>
2023-03-05 19:08:18 +0530

    local cleanup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+function App() {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+        <div className="px-6 py-4">
+          <div className="font-bold text-purple-100 text-xl mb-2">sam</div>
+          <ul>
+            <li><strong>Views:</strong>400</li>
+            <li><strong>Downloads:</strong>400</li>
+            <li><strong>Likes:</strong>400</li>
+          </ul>
+        </div>        
+    </div>
+  );
+}
+
+export default App;
